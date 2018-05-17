baa1f749664f02694fbb4e24bc81290aa9eb0fae
Benilyn <benilyn.gonzales@gmail.com>
2018-05-17 03:09:47 -0700

    updated Schema to add project on fields

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
 const projectSchema = mongoose.Schema({
-	title:{type: String, required: true},
-	dueDate: {type: Date},
-	details:{type: String},
+	projectTitle:{type: String, required: true},
+	projectDueDate: {type: Date},
+	projectDetail:{type: String},
 	projectTask: {type: String}
 });
 
 projectSchema.methods.apiRepr = function() {
 	return {
 		id: this._id,
-		title: this.title,
-		dueDate: this.dueDate,
-		details: this.details,
+		projectTitle: this.projectTitle,
+		projectDueDate: this.projectDueDate,
+		projectDetail: this.projectDetail,
 		projectTask: this.projectTask
 	};
 };

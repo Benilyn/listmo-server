2d8386089f38c40ca12c05bd73f854a72921b7b9
Benilyn <benilyn.gonzales@gmail.com>
2018-05-24 00:29:56 -0700

    added code for user login

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const fs = require('fs');
 const morgan = require('morgan');
+
 
 const userRouter = require('./router/userRouter');
 const projectRouter = require('./router/projectRouter');
 const taskRouter = require('./router/taskRouter');
+const loginRouter = require('./router/loginRouter');
 
 mongoose.Promise = global.Promise;
 
@@ -14,15 +17,77 @@ const {User} = require('./models/userModel');
 const {Project} = require('./models/projectModel');
 const {Task} = require('./models/taskModel');
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 const app = express();
 
 // logging
+app.use(morgan('dev'));
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use('/user', userRouter);
 app.use('/project', projectRouter);
 app.use('/task', taskRouter);
+//app.use('/login', loginRouter);
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  })
+}));
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+//---------------------------------
+// start of login
+//--------------------------------
+
+const localStrategy = require('passport-local').Strategy;
+passport.use(new localStrategy(  
+    {
+        usernameField: 'userName',
+        passwordField: 'password'
+    },
+    function(userName, password, done) {
+        User.findOne({ userName: userName })
+          .then(function(user) {
+              if (!user || !user.password === password) {
+                  return done(null, false, { message: 'Incorrect email or password.' });
+              }
+              done(null, user);
+          }); //.then function
+    } //function(email, password, done)
+)); //passport.use
+
+passport.serializeUser(function(user, done) {  
+    done(null, user.id);
+}); //passport.serializeUser
+
+passport.deserializeUser(function(id, done) {  
+    User.findOne({ _id: id })
+      .then(function(user) {
+          done(null, user);
+      }) //.then function
+    .catch(function(err) {
+        done(err, null);
+    }); //.catch function
+}); //passport.deserializeUser
+
+app.post ('/login', passport.authenticate('local'), function (req, res) {
+  res.json(req.user.apiRepr());
+}); //router.post
+
+//---------------------------------
+// end of login
+//--------------------------------
+
 
 app.use('*', function(req, res) {
   return res.status(404).json({message: 'Not Found'});
@@ -67,6 +132,17 @@ function closeServer() {
 if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
+
+app.get('/login', (req, res) => {
+  if (!(req.user)) {
+    const message = 'No user logged in';
+    console.error(message);
+    res.sendStatus(401);
+  }
+  else{
+      res.sendStatus(200);
+  }
+});
 
 module.exports = {app, runServer, closeServer};
 

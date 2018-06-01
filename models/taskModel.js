50d939fe863413b00d817c156281f202b65eda1f
Benilyn <benilyn.gonzales@gmail.com>
2018-06-01 11:33:51 -0700

    added taskProject to TaskSchema and TaskSchema.apiRepr

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const Project = require('./projectModel.js'); 
+const Project = require('./projectModel.js');
 
 const TaskSchema = mongoose.Schema({
-//	project: {type: mongoose.Schema.ObjectId, ref: 'Project'},
+	taskProject: {type: mongoose.Schema.ObjectId, ref: 'Project'},
 	taskTitle: {type: String},
 	taskDueDate: {type: Date},
 	taskDetail:{type: String}
@@ -10,7 +10,7 @@ const TaskSchema = mongoose.Schema({
 
 TaskSchema.methods.apiRepr = function() {
 	return {
-//		project: this.project,
+		taskProject: this.taskProject,
 		id: this._id,
 		taskTitle: this.taskTitle,
 		taskDueDate: this.taskDueDate,

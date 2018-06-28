0d6bb921850ed15f29d203ad4483650a4576a44b
Benilyn <benilyn.gonzales@gmail.com>
2018-06-28 14:29:09 -0700

    export JWT_SECRET and JWT_EXPIRY

--- config.js
+++ config.js
@@ -6,3 +6,6 @@ exports.DATABASE_URL =	process.env.DATABASE_URL ||
 
 exports.TEST_DATABASE_URL =	process.env.TEST_DATABASE_URL ||
 						'mongodb://localhost/test-listmo-server';
+
+exports.JWT_SECRET = process.env.JWT_SECRET || 'abc';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';

54eddbb58b028e2edb9c10999d265e744bd5bb04
Benilyn <benilyn.gonzales@gmail.com>
2018-07-23 11:31:22 -0700

    console.log authToken for testing purpose

--- router/authRouter.js
+++ router/authRouter.js
@@ -20,6 +20,7 @@ const localAuth = passport.authenticate('local', {session: false});
 // The user provides a username and password to login
 router.post('/', localAuth, (req, res) => {
   const authToken = createAuthToken(req.user.serialize());
+  console.log(authToken);
   res.json({authToken});
 });
 
